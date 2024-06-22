362a5bb55a2f13c94bd156a1dc813a92d8225826
dong <dejoker95@gmail.com>
2024-06-22 22:38:12 +0900

    add package 'tool' & main.py

--- tools/news_crawler.py
+++ tools/news_crawler.py
@@ -1,10 +1,8 @@
-
-# request에 에러처리 넣기 
-
+import pytz
 import requests
-from datetime import datetime
+from datetime import date, datetime, timedelta
 
-class NaverCrawler:
+class NaverApiCrawler:
 
     def __init__(self, client_id, client_secret, endpoint):
         self.client_id = client_id
@@ -22,10 +20,12 @@ class NaverCrawler:
         response = requests.get(url=self.endpoint, headers=self.headers, params=params)
         return response.json()['items']
 
-    def get_articles_since_date(self, keyword, date):
+    def get_articles_since_date(self, keyword, days):
         articles = []
         it = 0
         done = False
+        since = self.get_date_week_ago(days)
+
         while not done:
             params = {
                 'query': keyword,
@@ -33,11 +33,12 @@ class NaverCrawler:
                 'start': (it * 100) + 1,
                 'sort': 'date'
             }
+
             response = requests.get(url=self.endpoint, headers=self.headers, params=params)
             if response.status_code == 200:
                 for article in response.json()['items']:
                     pubdate = self.pubdate_to_datetime(article['pubDate'])
-                    if pubdate < date:
+                    if pubdate < since:
                         done = True
                         break
                     articles.append(article)
@@ -49,24 +50,35 @@ class NaverCrawler:
     def transform_articles(self, articles):
         articles = self.add_article_id(articles)
         articles = self.filter_naver_domain(articles)
+        articles = self.add_has_content_flag(articles)
         return articles
 
+    # add ID in MongoDB format
     def add_article_id(self, articles):
-        for keyword, items in articles.items():
-            for item in items:
-                id = ''.join(item['link'].split('/')[-2:])
-                if '?' in id:
-                    id = id.split('?')[0]
-                item['id'] = id
+        for article in articles:
+            id = ''.join(article['link'].split('/')[-2:])
+            if '?' in id:
+                id = id.split('?')[0]
+            article['_id'] = id
         return articles
 
     def filter_naver_domain(self, articles):
         condition = lambda x: x['originallink'] != x['link']
-        for keyword, val in articles.items():
-            articles[keyword] = [x for x in val['items'] if condition(x)]
-        return articles
+        return [x for x in articles if condition(x)]
 
     def pubdate_to_datetime(self, pubdate):
         return datetime.strptime(pubdate, '%a, %d %b %Y %H:%M:%S %z')
+
+    def get_date_week_ago(self, days):
+        today = datetime.today().replace(tzinfo=pytz.timezone('Asia/Seoul'))
+        today.replace(hour=0, minute=0)
+        week_ago = today - timedelta(days=days)
+        return week_ago
+
+    def add_has_content_flag(self, articles):
+        for article in articles:
+            article['has_content'] = False
+        return articles
+
     
         
